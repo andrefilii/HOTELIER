406579c982a7368db1b0f5602a1779565aefc7d8
Andrea Filippi <andrea.filippi02@outlook.it>
2024-01-02 19:11:03 +0100

    - creazione file di esempio

---
+++ src/entities/Hotel.java
@@ -0,0 +1,95 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Hotel implements Serializable {
+    private static final long serialVersionUID = 1;
+
+    private Integer id;
+    private String name;
+    private String description;
+    private String city;
+    private String phone;
+    private List<String> services;
+    private Double rate;
+    private Ratings ratings;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<String> getServices() {
+        return services;
+    }
+
+    public void setServices(List<String> services) {
+        this.services = services;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
+
+    public Ratings getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(Ratings ratings) {
+        this.ratings = ratings;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", city='" + city + '\'' +
+                ", phone='" + phone + '\'' +
+                ", services=" + services +
+                ", rate=" + rate +
+                ", raitings=" + ratings +
+                '}';
+    }
+}
---
+++ src/entities/Ratings.java
@@ -0,0 +1,54 @@
+package entities;
+
+import java.io.Serializable;
+
+public class Ratings implements Serializable {
+    private static final long serialVersionUID = 1;
+
+    private Double cleaning;
+    private Double position;
+    private Double services;
+    private Double quality;
+
+    public Double getCleaning() {
+        return cleaning;
+    }
+
+    public void setCleaning(Double cleaning) {
+        this.cleaning = cleaning;
+    }
+
+    public Double getPosition() {
+        return position;
+    }
+
+    public void setPosition(Double position) {
+        this.position = position;
+    }
+
+    public Double getServices() {
+        return services;
+    }
+
+    public void setServices(Double services) {
+        this.services = services;
+    }
+
+    public Double getQuality() {
+        return quality;
+    }
+
+    public void setQuality(Double quality) {
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
+        return "Ratings{" +
+                "cleaning=" + cleaning +
+                ", position=" + position +
+                ", services=" + services +
+                ", quality=" + quality +
+                '}';
+    }
+}
